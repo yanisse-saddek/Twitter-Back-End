1e2ee51e36a52c814ece80d0d59df1b0f304de48
Ismail51 <dassler-ben@hotmail.fr>
2022-05-10 16:34:04 +0200

    controllers

---
+++ Controllers/profil.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const userModel = require('../Models/UserModel')
+const express = require('express')
+const router = express.Router()
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id
+  console.log(id)
+  userModel.findById(id).exec().then(data => {
+    res.json(data)
+  }).catch(err => {
+    console.log(err);
+    res.status(400).json('invalid')
+  })
+})
+
+
+
+
+module.exports = router
+
